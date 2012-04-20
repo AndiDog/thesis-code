3b1d7df457bf35f4ab3b9ed563a48b5b204e7435
Andreas Sommer <andreas.sommer87@googlemail.com>
2012-04-20 08:36:56 +0100

    Sample application / Titanium: Removed test data

--- Sample application/Titanium/Resources/ui/common/OldOrdersTab.js
+++ Sample application/Titanium/Resources/ui/common/OldOrdersTab.js
@@ -92,28 +92,8 @@ function OldOrdersTab()
         backgroundColor: '#000'
     });
 
-    var testData = [
-        {title: 'Row 1'},
-        {title: 'Row 2'},
-        {title: 'Row 3'},
-        {title: 'Row 4'},
-        {title: 'Row 5'},
-        {title: 'Row 6'},
-        {title: 'Row 7'},
-        {title: 'Row 8'},
-        {title: 'Row 9'},
-        {title: 'Row 10'},
-        {title: 'Row 11'},
-        {title: 'Row 12'}
-    ]
-
-    for(var i = 0; i < 12; ++i)
-        testData[i] = {title: 'Row ' + (i+1),
-                       hasDetail: true}
-
     this.table = new Titanium.UI.createTableView({
-        headerTitle: String.format(L('oldOrdersInTotal'), testData.length),
-        data: testData,
+        headerTitle: '...',
         scrollable: true
     })
 
