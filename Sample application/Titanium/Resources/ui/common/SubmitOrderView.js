0c7f54c19812a6d20c340ac8c4a18e482abec43c
Andreas Sommer <andreas.sommer87@googlemail.com>
2012-04-28 13:11:39 +0100

    Sample application / Titanium: Fixes for order submission screen, retrieval of stores by entered location

--- Sample application/Titanium/Resources/ui/common/SubmitOrderView.js
+++ Sample application/Titanium/Resources/ui/common/SubmitOrderView.js
@@ -1,15 +1,30 @@
+var moment = require('/lib/moment')
+
 function SubmitOrderView(order)
 {
     if(!(this instanceof SubmitOrderView))
         return new SubmitOrderView(order)
 
-    this.updateSearchBar = function()
+    this.setLocation = function()
     {
-        var foundStores = [
-            {id: 1, name: "Walmart", "address": "Marienplatz 1, München"},
-            {id: 2, name: "LITTLE", "address": "Leopoldstra\u00dfe 144, M\u00fcnchen"}
-        ]
+        Ti.Geolocation.purpose = 'Find nearest stores'
+        Ti.Geolocation.setAccuracy(Ti.Geolocation.ACCURACY_KILOMETER)
 
+        var _this = this
+
+        Ti.Geolocation.getCurrentPosition(function(e) {
+            if(!e.success)
+            {
+                alert(String.format(L('couldNotFindLocation'), e.error.message))
+                return
+            }
+
+            _this.searchBar.setValue(String.format('%.5f;%.5f', e.coords.latitude, e.coords.longitude))
+        })
+    }
+
+    this.setStores = function(foundStores)
+    {
         var storeIds = {}
 
         for(var i = 0; i < foundStores.length && i < 5; ++i)
@@ -55,12 +70,74 @@ function SubmitOrderView(order)
         var existingRowsCount = this.pickUpLocationsTable.data.length ? this.pickUpLocationsTable.data[0].rows.length : 0
         for(var i = existingRowsCount - 1; i >= 0; --i)
             if(this.pickUpLocationsTable.data[0].rows[i].customData && !(this.pickUpLocationsTable.data[0].rows[i].customData.storeId in storeIds))
-                this.pickUpLocationsTable.deleteRow(this.pickUpLocationsTable.data[0].rows[i])
+                this.pickUpLocationsTable.deleteRow(i)
+    }
+
+    this.updateSearchBar = function(delay)
+    {
+        var search = this.searchBar.value
+
+        if(search == this.lastRequest[0])
+            return
+
+        // We don't want to send a request for every character typed, that's what the "delay" parameter is for
+        if((delay && moment().diff(this.lastRequest[1]) < 2500) || this.hasPendingRequest)
+        {
+            var _this = this
+
+            setTimeout(function() { _this.updateSearchBar(true) }, 500)
+
+            return
+        }
+
+        var latLngMatch = /^(\d\.\d+);(\d\.\d+)$/.exec(search)
+        var parameters
+
+        if(latLngMatch)
+            parameters = "lat=" + latLngMatch[1] + "&lng=" + latLngMatch[2]
+        else
+            parameters = "loc=" + encodeURIComponent(search)
+
+        Ti.App.Properties.setString('storesSearch', search)
+
+        var _this = this
+
+        var client = Ti.Network.createHTTPClient({
+            onload: function(e) {
+                _this.lastRequest = [search, moment()]
+                _this.hasPendingRequest = false
+
+                var list = JSON.parse(this.responseText)
+                var stores = list['stores']
+
+                Ti.App.Properties.setList('stores', stores)
+
+                _this.setStores(stores)
+            },
+            onerror: function(e) {
+                _this.hasPendingRequest = false
+
+                Ti.API.error(e.error)
+                // TODO: replace by notification?!
+                alert('Error retrieving list of nearest stores' + e.error)
+
+                var cachedStores = Ti.App.Properties.getList('stores', null)
+                if(cachedStores != null)
+                    _this.setStores(cachedStores)
+            },
+            timeout: 5000
+        })
+
+        client.open('GET', Ti.App.globals.webServiceBaseUri + 'stores/by-location/?' + parameters)
+        this.hasPendingRequest = true
+        client.send()
     }
 
     var _this = this
 
     this.order = order
+    this.lastRequest = ['', moment().subtract('days', 1)]
+    this.hasPendingRequest = false
 
     var self = Ti.UI.createWindow({
         title: L('submitOrder'),
@@ -90,11 +167,12 @@ function SubmitOrderView(order)
         touchEnabled: false
     }))
 
-    var searchBar = Ti.UI.createSearchBar({
+    this.searchBar = Ti.UI.createSearchBar({
         showCancel: false,
         width: Ti.UI.FILL,
         height: 40,
-        top: 6
+        top: 6,
+        hintText: L('searchStoresHint')
     })
 
     this.pickUpLocationsTable = Ti.UI.createTableView({
@@ -103,7 +181,7 @@ function SubmitOrderView(order)
         scrollable: false
     })
 
-    scrollView.add(searchBar)
+    scrollView.add(this.searchBar)
     scrollView.add(this.pickUpLocationsTable)
 
     // Create username/password form
@@ -142,11 +220,12 @@ function SubmitOrderView(order)
     verticalView.add(horizontalView2)
     scrollView.add(verticalView)
 
-    searchBar.addEventListener('change', function() { _this.updateSearchBar() })
-    searchBar.addEventListener('return', function() { _this.updateSearchBar() })
+    this.searchBar.setValue(Ti.App.Properties.getString('storesSearch', ''))
+    this.searchBar.addEventListener('change', function() { _this.updateSearchBar(true) })
+    this.searchBar.addEventListener('return', function() { _this.updateSearchBar() })
 
-    // TEST
-    setTimeout(function() { _this.updateSearchBar() }, 200)
+    if(Ti.Geolocation.locationServicesEnabled)
+        setTimeout(function() { _this.setLocation() }, 1)
 
     return self
 }
