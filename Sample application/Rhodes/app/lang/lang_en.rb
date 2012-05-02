e10b53bffea94df2a0b73bf9dd1c866d4734176e
Andreas Sommer <andreas.sommer87@googlemail.com>
2012-05-02 13:52:47 +0100

    Sample application / Rhodes: Old order screen design

--- Sample application/Rhodes/app/lang/lang_en.rb
+++ Sample application/Rhodes/app/lang/lang_en.rb
@@ -2,6 +2,7 @@ module Localization
 Views = {
   :add_pictures => 'Add pictures',   
   :current_order => 'Current order',
-  :old_orders  => 'Old orders'
+  :old_orders  => 'Old orders',
+  :old_orders_in_total => 'There are %d old orders in total:'
 }
 end
