e4bb9bb7554432770d953c0809f05ccb329ff91b
Andreas Sommer <andreas.sommer87@googlemail.com>
2012-05-03 21:18:48 +0100

    Sample application / Rhodes: Caching of thumbnails

--- Sample application/Rhodes/app/helpers/thumbnails.rb
+++ Sample application/Rhodes/app/helpers/thumbnails.rb
@@ -1,6 +1,29 @@
+require 'json'
+
 module Thumbnails
   def on_get_thumbnail
-    # @params['status'] != 'ok'
+    return if @params['status'] != 'ok'
+
+    # to_i to ensure we actually got the ID
+    pictureId = @params['pictureId'].to_i
+
+    expiresHeader = @params['headers']['expires']
+
+    if expiresHeader
+      # Parsing of format "Thu, 15 Apr 2010 20:00:00 GMT" works directly with Time.parse
+      expires = Time.parse(expiresHeader)
+    else
+      # One hour by default
+      expires = Time.now + 3600
+    end
+
+    filename = get_thumbnail_filename(pictureId) + '.json'
+    f = File.open(filename, 'wb')
+    begin
+      f.write(JSON.generate({:expires => expires}))
+    ensure
+      f.close()
+    end
   end
 
   def get_thumbnail_filename(pictureId, fallback_if_not_exists=false)
@@ -16,6 +39,28 @@ module Thumbnails
   def start_thumbnail_downloads(allPictureIds)
     for pictureId in allPictureIds
       filename = get_thumbnail_filename(pictureId)
+      infoFilename = filename + '.json'
+
+      if File.exists?(infoFilename)
+        f = File.open(infoFilename, 'rb')
+        begin
+          content = f.read()
+        ensure
+          f.close()
+        end
+
+        info = Rho::JSON.parse(content)
+
+        if Time.parse(info['expires']) > Time.now
+          puts "Cache hit for thumbnail #{pictureId}"
+          next
+        end
+
+        File.delete(filename) if File.exists?(filename)
+        File.delete(infoFilename)
+      end
+
+      puts "Reloading thumbnail #{pictureId} (cache miss)"
 
       Rho::AsyncHttp.download_file(
         :url => "http://andidogs.dyndns.org/thesis-mobiprint-web-service/picture/#{pictureId}/thumbnail/?size=120",
