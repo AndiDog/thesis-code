f7320adf0eb7827902a12b3c06af19b34acebb99
Andreas Sommer <andreas.sommer87@googlemail.com>
2012-05-08 14:30:47 +0100

    Sample application / Rhodes: Failed attempt to workaround file upload issue

--- Sample application/Rhodes/app/helpers/picture_upload.rb
+++ Sample application/Rhodes/app/helpers/picture_upload.rb
@@ -1,7 +1,52 @@
+require 'net/http'
+
 class PictureUpload
   @@uploading = {}
 
-  def is_uploading?(filename)
+  def self.is_uploading?(filename)
     @@uploading.has_key?(filename)
+  end
+
+  def self.on_upload_finished(filename)
+    @@uploading.delete(filename)
+  end
+
+  def self.upload_picture(filename, callback)
+    return if @@uploading.has_key?(filename)
+
+    @@uploading[filename] = true
+
+    begin
+      Thread.new do
+        begin
+          RestClient.put(
+            'http://andidogs.dyndns.org/thesis-mobiprint-web-service/pictures/',
+            :picture => filename
+          )
+
+          callback.call(filename)
+        rescue => e
+          puts "Failed to upload picture: #{e.response}"
+        ensure
+          self.on_upload_finished
+        end
+      end
+=begin
+      Rho::AsyncHttp.post(
+        :http_command => 'PUT',
+        :url => 'http://andidogs.dyndns.org/thesis-mobiprint-web-service/pictures/',
+        :callback => callback,
+        :multipart => [
+        {
+          :filename => filename,
+          :name => "picture",
+          :content_type => "application/octet-stream"
+        }]
+      )
+=end
+    rescue
+      @@uploading.delete(filename)
+      raise
+    end
   end
 end
