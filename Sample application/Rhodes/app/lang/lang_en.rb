47efede7b9615c8c1a10e0fabea832c1d49453fe
Andreas Sommer <andreas.sommer87@googlemail.com>
2012-05-11 09:42:55 +0100

    Sample application / Rhodes: Designed order submission, small style changes

--- Sample application/Rhodes/app/lang/lang_en.rb
+++ Sample application/Rhodes/app/lang/lang_en.rb
@@ -1,6 +1,7 @@
 module Localization
 Views = {
   :add_pictures => 'Add pictures',
+  :confirm_order => 'Confirm order',
   :current_order => 'Current order',
   :must_add_pictures => 'You did not add any pictures.',
   :n_pictures => '%d pictures',
@@ -8,10 +9,12 @@ Views = {
   :old_orders => 'Old orders',
   :old_orders_in_total => 'There are %d old orders in total:',
   :order_has_n_pictures => 'This order contains %d pictures:',
+  :password => 'Password',
   :submission_title => 'To submit your order of %d pictures, select a pick-up location close to you and enter your details. Enter an address to show stores near you:',
   :submit => 'Submit',
   :submit_order => 'Submit order',
   :upload => 'Upload',
+  :username => 'Username',
 
   # States
   :printed => 'Printed',
