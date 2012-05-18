c1fa0511d66c3ee9428681c0168af0c8e471984a
Andreas Sommer <andreas.sommer87@googlemail.com>
2012-05-18 20:32:19 +0100

    Sample application / Rhodes: Removed debug code for picture scan

--- Sample application/Rhodes/app/helpers/picture_scan.rb
+++ Sample application/Rhodes/app/helpers/picture_scan.rb
@@ -1,12 +1,10 @@
 module PictureScan
   def scanPictures
-    return [['/sdcard/Muenchen', 2], ['/sdcard/Rage Comics', 47]]
-
     ret = []
 
     _scanPictures(ret, '/sdcard', 0)
 
-    puts "SCAN RESULT: #{ret}"
+    puts "Scan result: #{ret}"
 
     ret
   end
@@ -21,14 +19,17 @@ module PictureScan
     for filename in Dir.new(directory).entries
       fullFilename = File.join(directory, filename)
 
-      puts "FULL:"
-      puts fullFilename
+      puts "Full filename #{fullFilename}"
       if filename =~ /\.jpg$/i and File.file?(fullFilename)
         resultForThisDir ||= [directory, 0]
         resultForThisDir[1] += 1
       elsif not filename =~ /^\./
         _scanPictures(results, fullFilename, recursionDepth + 1)
       end
+    end
+
+    if resultForThisDir
+      results << resultForThisDir
     end
   end
 end
