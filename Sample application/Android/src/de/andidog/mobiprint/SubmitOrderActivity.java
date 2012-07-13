d8dc7b6c738ba46783935ce05709a55d45d9a268
Andreas Sommer <andreas.sommer87@googlemail.com>
2012-07-13 15:04:07 +0100

    Sample application / Android: Location caching

--- Sample application/Android/src/de/andidog/mobiprint/SubmitOrderActivity.java
+++ Sample application/Android/src/de/andidog/mobiprint/SubmitOrderActivity.java
@@ -1,11 +1,21 @@
 package de.andidog.mobiprint;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 import android.app.Activity;
 import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +24,7 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SubmitOrderActivity extends Activity
 {
@@ -56,6 +67,48 @@ public class SubmitOrderActivity extends Activity
         locations.addView(locationLayout);
     }
 
+    /**
+     * @return May return NULL.
+     */
+    private String getCachedLocation()
+    {
+        File cacheDir = getCacheDir();
+        File cachedFile = new File(cacheDir.getAbsolutePath(), "location.txt");
+        FileInputStream stream = null;
+
+        try
+        {
+            boolean bam=cachedFile.exists();
+            long lbam=cachedFile.length();
+
+            if(cachedFile.exists() && cachedFile.length() > 5)
+            {
+                stream = new FileInputStream(cachedFile);
+                InputStreamReader in = new InputStreamReader(stream);
+
+                // Fair enough :D
+                char[] buffer = new char[(int)cachedFile.length()*4];
+                int len = in.read(buffer, 0, buffer.length);
+                return String.valueOf(buffer, 0, len).trim();
+            }
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+            Toast.makeText(this, "Failed to read cached location: " + e, Toast.LENGTH_LONG).show();
+        }
+
+        return null;
+    }
+
+    private String locationToString(Location location)
+    {
+        if(location == null)
+            return null;
+
+        return String.format("%.5f;%.5f", location.getLatitude(), location.getLongitude());
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
@@ -86,6 +139,44 @@ public class SubmitOrderActivity extends Activity
         addLocationRadioButton(new Store(2, "F-Markt", "somewhere else"));
 
         startLocating();
+
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params)
+            {
+                try
+                {
+                    Thread.sleep(500);
+                }
+                catch(InterruptedException e)
+                {
+                }
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void result)
+            {
+                locationEditText.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void afterTextChanged(Editable s)
+                    {
+                        setCurrentLocation(s.toString());
+                    }
+
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after)
+                    {
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count)
+                    {
+                    }
+                });
+            }
+        }.execute();
     }
 
     private synchronized void setCurrentLocation(String currentLocation)
@@ -97,12 +188,35 @@ public class SubmitOrderActivity extends Activity
         {
             locationEditText.setText(this.currentLocation);
             updateStores();
-        }
-    }
 
-    private void setCurrentLocation(Location location)
-    {
-        setCurrentLocation(String.format("%.5f;%.5f", location.getLatitude(), location.getLongitude()));
+            if(this.currentLocation.length() > 0)
+            {
+                File cacheDir = getCacheDir();
+                File cachedFile = new File(cacheDir.getAbsolutePath(), "location.txt");
+                FileOutputStream stream = null;
+
+                try
+                {
+                    stream = new FileOutputStream(cachedFile);
+                    stream.write(this.currentLocation.getBytes());
+                }
+                catch(IOException e)
+                {
+                    e.printStackTrace();
+                    Toast.makeText(this, "Failed to store cached location: " + e, Toast.LENGTH_LONG).show();
+
+                    try
+                    {
+                        if(stream != null)
+                            stream.close();
+                    }
+                    catch(IOException e2)
+                    {
+                        e2.printStackTrace();
+                    }
+                }
+            }
+        }
     }
 
     private void startLocating()
@@ -114,7 +228,7 @@ public class SubmitOrderActivity extends Activity
             public void onLocationChanged(Location location)
             {
                 Log.i(TAG, "Found coarse location");
-                setCurrentLocation(location);
+                setCurrentLocation(locationToString(location));
                 locationManager.removeUpdates(locationListener);
             }
 
@@ -153,8 +267,15 @@ public class SubmitOrderActivity extends Activity
         Location cachedLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
         if(cachedLocation == null)
             cachedLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-        if(cachedLocation != null && System.currentTimeMillis() - cachedLocation.getTime() < 86400000)
-            setCurrentLocation(cachedLocation);
+
+        String cachedLocationStr;
+
+        if(cachedLocation != null && System.currentTimeMillis() - cachedLocation.getTime() > 1000 * 120)
+            cachedLocationStr = getCachedLocation();
+        else
+            cachedLocationStr = locationToString(cachedLocation);
+
+        setCurrentLocation(cachedLocationStr == null ? "" : cachedLocationStr);
 
         locationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, locationListener, null);
         locationManager.requestSingleUpdate(LocationManager.NETWORK_PROVIDER, locationListener, null);
