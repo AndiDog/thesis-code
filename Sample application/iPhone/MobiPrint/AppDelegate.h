17ee67c0e752ce67b820a165c163d0c6edb5bd49
Andreas Sommer <andreas.sommer87@googlemail.com>
2012-07-17 15:59:23 +0100

    Sample application / iPhone: Created basic project with tab bar and three navigation controllers

---
+++ Sample application/iPhone/MobiPrint/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  MobiPrint
+//
+//  Created by Dog on 17/07/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
